a0829401b5f3d8567ea23d0c49df20fcdaea0a99
liukong1220 <1625038134@qq.com>
2026-05-10 21:57:47 +0800

    代码修复

--- src/sentry_debug.cpp
+++ src/sentry_debug.cpp
@@ -110,7 +110,7 @@ DebugMode debug_mode_from_gimbal_mode(io::GimbalMode mode)
     case io::GimbalMode::AUTO_AIM:
     case io::GimbalMode::IDLE:
     default:
-      return DebugMode::IDLE;
+      return DebugMode::AutoAim;
   }
 }
 
